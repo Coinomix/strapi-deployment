0e5e32c149b15aecaa1a6077d9ded3ef5ab4ba90
Maksym Didyk <m.didyk@itg-investments.com>
2023-10-11 17:03:55 +0300

    updated strapi

--- config/plugins.ts
+++ config/plugins.ts
@@ -14,19 +14,5 @@ export default ({ env }) => ({
       },
     },
   },
-  "netlify-deployments": {
-    enabled: true,
-    config: {
-      accessToken: "RXdxKWiQLwq7z0rKAiIpKnNboFmfwA-3MOwp9dRb3LE",
-      sites: [
-        {
-          name: 'Site 1',
-          id: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-          buildHook: "https://api.netlify.com/build_hooks/<hook_id>",
-          branch: 'main' // optional
-        }
-      ]
-    },
-  },
   // ...
 });
